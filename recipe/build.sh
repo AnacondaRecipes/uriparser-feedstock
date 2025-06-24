2ca62b7125217c07f5679bba8d4b80ea4c92e000
PatrykKups <pkups@anaconda.com>
2025-06-24 17:25:01 +0200

    uriparser 0.9.8  (#2)
    * PKG-8523: uriparser 0.9.8

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,12 +13,16 @@ cmake -GNinja \
       -DCMAKE_C_FLAGS="$CFLAGS" \
       -DCMAKE_POSITION_INDEPENDENT_CODE=on \
       -DURIPARSER_BUILD_DOCS=off \
-      -DURIPARSER_BUILD_TESTS=off \
       -DURIPARSER_BUILD_TOOLS=off \
-      -DURIPARSER_BUILD_WCHAR_T=off \
       -DBUILD_SHARED_LIBS=on \
+      -DURIPARSER_BUILD_TESTS=on \
+      -DURIPARSER_BUILD_WCHAR_T=on \
+      -DURIPARSER_BUILD_CHAR=on \
       ..
 
 cmake --build . --config Release --target install
+
+# CMAKE: URIPARSER_BUILD_TESTS=ON requires both URIPARSER_BUILD_CHAR=ON and URIPARSER_BUILD_WCHAR_T=ON.
+ctest --output-on-failure -j${CPU_COUNT}
 
 popd
